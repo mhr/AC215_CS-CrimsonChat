199b28f8ee36a5e04e4acc2108da9afa261e3e12
Mathew Retchin <mhr2145@columbia.edu>
2024-12-05 19:58:54 -0500

    /run-model-training

--- src/model_training/docker-shell.sh
+++ src/model_training/docker-shell.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# exit immediately if a command exits with a non-zero status
+# Exit immediately if a command exits with a non-zero status
 set -e
 
 # Read the settings file
@@ -11,10 +11,11 @@ export IMAGE_NAME="model_training"
 # Build the image based on the Dockerfile
 docker build -t $IMAGE_NAME -f Dockerfile .
 
+# Detect if a TTY is available
 if [ -t 1 ]; then
   DOCKER_FLAGS="-ti"
 else
-  DOCKER_FLAGS=""
+  DOCKER_FLAGS="-i"  # Use non-interactive mode when no TTY is present
 fi
 
 if [[ "$1" == "--run-cli" ]]; then
@@ -27,7 +28,7 @@ if [[ "$1" == "--run-cli" ]]; then
     -e BUCKET_NAME=$BUCKET_NAME \
     -e LOCATION=$LOCATION \
     -e MODEL_ENDPOINT=$MODEL_ENDPOINT \
-    $IMAGE_NAME python /app/cli.py --train --train_config /app/train_config.json --dataset /app/kaggle_mental_dataset.json
+    $IMAGE_NAME pipenv run python /app/cli.py --train --train_config /app/train_config.json --dataset /app/kaggle_mental_dataset.json
 else
   # Default: Drop into the container's shell because we're local
   docker run --rm --name $IMAGE_NAME $DOCKER_FLAGS \
