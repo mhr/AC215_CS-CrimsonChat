8ac7ca30b131ba39386b951daa4cb154d1825ac5
Mathew Retchin <mhr2145@columbia.edu>
2024-12-05 19:44:34 -0500

    trying to get ci-cd.yml working with a deploy command

--- src/model_training/docker-shell.sh
+++ src/model_training/docker-shell.sh
@@ -11,13 +11,26 @@ export IMAGE_NAME="model_training"
 # Build the image based on the Dockerfile
 docker build -t $IMAGE_NAME -f Dockerfile .
 
-# Run Container
-docker run --rm --name $IMAGE_NAME -ti \
--v "$BASE_DIR":/app \
--v "$SECRETS_DIR":/secrets \
--e GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
--e GCP_PROJECT=$GCP_PROJECT \
--e BUCKET_NAME=$BUCKET_NAME \
--e LOCATION=$LOCATION \
--e MODEL_ENDPOINT=$MODEL_ENDPOINT \
-$IMAGE_NAME
+if [[ "$1" == "--run-cli" ]]; then
+  # Run the container and execute the Python CLI
+  docker run --rm --name $IMAGE_NAME -ti \
+    -v "$BASE_DIR":/app \
+    -v "$SECRETS_DIR":/secrets \
+    -e GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
+    -e GCP_PROJECT=$GCP_PROJECT \
+    -e BUCKET_NAME=$BUCKET_NAME \
+    -e LOCATION=$LOCATION \
+    -e MODEL_ENDPOINT=$MODEL_ENDPOINT \
+    $IMAGE_NAME python /app/cli.py --train --train_config /app/train_config.json --dataset /app/kaggle_mental_dataset.json
+else
+  # Default: Drop into the container's shell
+  docker run --rm --name $IMAGE_NAME -ti \
+    -v "$BASE_DIR":/app \
+    -v "$SECRETS_DIR":/secrets \
+    -e GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
+    -e GCP_PROJECT=$GCP_PROJECT \
+    -e BUCKET_NAME=$BUCKET_NAME \
+    -e LOCATION=$LOCATION \
+    -e MODEL_ENDPOINT=$MODEL_ENDPOINT \
+    $IMAGE_NAME
+fi
