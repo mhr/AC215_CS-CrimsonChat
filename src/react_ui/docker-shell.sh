d18d98e88ee5185b392fc9abe5aca85b4615ffdf
artemislong <artemdinh@gmail.com>
2024-12-07 18:48:07 -0500

    ReactUI changed backend path to api

--- src/react_ui/docker-shell.sh
+++ src/react_ui/docker-shell.sh
@@ -6,7 +6,7 @@ export IMAGE_NAME="crimson-chat-ui"
 export IMAGE_TAG="latest"
 
 # Set build-time arguments (update REACT_APP_API_URL as needed)
-REACT_APP_API_URL="http://35.237.221.78:9000"
+REACT_APP_API_URL="http://35.237.221.78/api"
 
 # Build the Docker image for amd64 architecture
 docker build --platform linux/amd64 \
