f0eec8712e5d2bb0634084cf21058d1f15dd4446
artemislong <artemdinh@gmail.com>
2024-12-08 16:21:50 -0500

    hardcoded qdrant api-service

--- src/api_service/routers/llm_chat_routers.py
+++ src/api_service/routers/llm_chat_routers.py
@@ -14,10 +14,12 @@ router = APIRouter()
 # Load environment variables
 GCP_PROJECT = os.getenv("GCP_PROJECT")
 LOCATION = os.getenv("LOCATION")
-QDRANT_URL = os.getenv("QDRANT_URL")
-QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
-MODEL_ENDPOINT = os.getenv("MODEL_ENDPOINT")
-
+# QDRANT_URL = os.getenv("QDRANT_URL")
+# QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
+# MODEL_ENDPOINT = os.getenv("MODEL_ENDPOINT")
+QDRANT_URL = "https://1494f517-c19c-490b-8a4e-43ff3b02bbb7.europe-west3-0.gcp.cloud.qdrant.io:6333"
+QDRANT_API_KEY = "5qJBIKdEycPYlWfaDiAwd-1Hz2z88qaBsSV_UAa4AljpqWpWGzmRTg"
+# MODEL_ENDPOINT = os.getenv("MODEL_ENDPOINT")
 
 # Initialize global dependencies
 qdrant_client = initialize_qdrant_client(QDRANT_URL, QDRANT_API_KEY)
